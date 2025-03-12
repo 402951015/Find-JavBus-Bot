157b97a42029e0fcc6339029f8722beed1799b33
402951015 <402951015@qq.com>
2025-03-12 21:41:05 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 export const ALLOWED_GROUPS = []
-export const BOT_TOKEN = '5045646424:AAGWgi85phnyyzsVZXChiuSxeV7X2Sjchn4'
-export const ROBOT_NAME = '@寻龙'
+export const BOT_TOKEN = '7932170022:AAFG59snzH9RJyrtKJhodxoYlQ0GBO8Z2c0'
+export const ROBOT_NAME = '@lansesBT_Bot'
